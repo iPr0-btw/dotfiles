6fbb4b40d4665a1489b70abb9b5e906ebe3df2d0
iPr0-btw <so23@duck.com>
2026-06-28 00:37:11 -0500

    Auto-commit: Sun Jun 28 12:37:11 AM CDT 2026

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -218,8 +218,8 @@ hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(menu))
 
 -- Window management
--- hl.bind(mainMod .. " + C", hl.dsp.window.close())
-hl.bind(mainMod .. " + C", hl.dsp.exec_cmd("kill -9 $(hyprctl activewindow -j | jq -r '.pid')"))
+hl.bind(mainMod .. " + C", hl.dsp.window.close())
+-- hl.bind(mainMod .. " + C", hl.dsp.exec_cmd("kill -9 $(hyprctl activewindow -j | jq -r '.pid')"))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 -- hl.bind(mainMod .. " + ENTER", hl.dsp.window.fullscreen({ all = false }))
