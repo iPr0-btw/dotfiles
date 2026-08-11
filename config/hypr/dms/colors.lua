6122800b81f2cd27cb4aae017b312e996600ae88
iPr0-btw <so23@duck.com>
2026-08-10 23:03:39 -0500

    Auto-commit: Mon Aug 10 11:03:39 PM CDT 2026

--- config/hypr/dms/colors.lua
+++ config/hypr/dms/colors.lua
@@ -4,20 +4,20 @@
 hl.config({
 	general = {
 		col = {
-			active_border = "rgb(fc03eb)",
+			active_border = "rgb(ffffff)",
 			inactive_border = "rgb(555555)",
 		},
 	},
 	group = {
 		col = {
-			border_active = "rgb(fc03eb)",
+			border_active = "rgb(ffffff)",
 			border_inactive = "rgb(555555)",
 			border_locked_active = "rgb(dd0000)",
 			border_locked_inactive = "rgb(555555)",
 		},
 		groupbar = {
 			col = {
-				active = "rgb(fc03eb)",
+				active = "rgb(ffffff)",
 				inactive = "rgb(555555)",
 				locked_active = "rgb(dd0000)",
 				locked_inactive = "rgb(555555)",
