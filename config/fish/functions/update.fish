c0ea941c096e3dac9b243cc881f3a071a1568e2a
iPr0-btw <so23@duck.com>
2026-08-26 12:35:29 -0500

    Auto-commit: Wed Aug 26 12:35:29 PM CDT 2026

--- config/fish/functions/update.fish
+++ config/fish/functions/update.fish
@@ -1,4 +1,4 @@
 function update --wraps='topgrade --disable shell -y' --description 'alias update=topgrade --disable shell -y'
     yay -Syu --noconfirm $argv
-    notify-send system updated $argv
+    notify-send system updated successfully. $argv
 end
