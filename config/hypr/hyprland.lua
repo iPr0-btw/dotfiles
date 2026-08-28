bdcdac40666311d07eeff89bacc4fb1696da0ee9
iPr0-btw <so23@duck.com>
2026-08-28 10:58:17 -0500

    Auto-commit: Fri Aug 28 10:58:17 AM CDT 2026

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -49,7 +49,7 @@ local menu = "rofi -show drun" --uncomment if you use this
 hl.on("hyprland.start", function()
 	hl.exec_cmd("swaybg -i ~/wall0.png")
 	hl.exec_cmd("dunst")
-	hl.exec_cmd("caelestia shell")
+	hl.exec_cmd("caelestia shell -d")
 	hl.exec_cmd("sleep 1 && hyprpaper")
 	-- hl.exec_cmd("hypridle")
 end)
