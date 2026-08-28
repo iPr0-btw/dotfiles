9dbf4f48a4bbc67396fca153e3b39fef291644ab
iPr0-btw <so23@duck.com>
2026-08-28 14:38:48 -0500

    Auto-commit: Fri Aug 28 02:38:48 PM CDT 2026

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -41,7 +41,7 @@ hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 local terminal = "kitty -o linux_display_server=wayland"
 local fileManager = "thunar"
 -- local menu = "dms ipc call spotlight-bar open"
-local menu = "rofi -show drun" --uncomment if you use this
+local menu = "rofi -show run"
 -------------------
 ---- AUTOSTART ----
 -------------------
