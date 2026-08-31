9a61d2ec9c9ab0dc877632760341493b87747933
iPr0-btw <so23@duck.com>
2026-08-31 12:24:10 -0500

    auto-committed at Mon 31 Aug 12:24:10 CDT 2026

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -40,7 +40,7 @@ hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 
 local terminal = "kitty -o linux_display_server=wayland"
 local fileManager = "thunar"
-local menu = "rofi -show drun"
+local menu = "caelestia shell drawers toggle lau"
 -------------------
 ---- AUTOSTART ----
 -------------------
