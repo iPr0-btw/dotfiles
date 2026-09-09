37aaf65c6bf81119be3d775ab13cf1f5c0d54afa
iPr0-btw <so23@duck.com>
2026-09-09 17:38:04 -0500

    auto-committed at Wed 09 Sep 17:38:04 CDT 2026

--- config/fish/functions/ff.fish
+++ config/fish/functions/ff.fish
@@ -1,9 +1,4 @@
 function ff
-    set distro (grep '^ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
-    switch $distro
-        case "why ever use microslop in 2026?"
-            fastfetch -c ~/.config/fastfetch/arch.jsonc $argv
-        case "*"
-            fastfetch -c ~/.config/fastfetch/config.jsonc $argv
-    end
+  fastfetch $argv
 end
+
