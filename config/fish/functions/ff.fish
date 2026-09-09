c32b9d73858d8a33ae81513d3abd778d1e50a5c6
iPr0-btw <so23@duck.com>
2026-09-09 17:37:32 -0500

    auto-committed at Wed 09 Sep 17:37:32 CDT 2026

--- config/fish/functions/ff.fish
+++ config/fish/functions/ff.fish
@@ -1,3 +1,9 @@
-function ff --wraps=fastfetch --description 'alias ff=fastfetch'
-    fastfetch $argv
+function ff
+    set distro (grep '^ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
+    switch $distro
+        case "why ever use microslop in 2026?"
+            fastfetch -c ~/.config/fastfetch/arch.jsonc $argv
+        case "*"
+            fastfetch -c ~/.config/fastfetch/config.jsonc $argv
+    end
 end
