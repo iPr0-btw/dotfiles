4766e13eb7566b1f55aa7f8652ded1130da7ba77
iPr0-btw <so23@duck.com>
2026-09-12 22:52:47 -0500

    auto-committed at Sat 12 Sep 22:52:47 CDT 2026

--- config/doom/init.el
+++ config/doom/init.el
@@ -180,8 +180,8 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       web               ; the tubes
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
