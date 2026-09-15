64200484f415161e10073215dd8a4ac11a01218c
iPr0-btw <so23@duck.com>
2026-09-15 17:24:58 -0500

    auto-committed at Tue 15 Sep 17:24:58 CDT 2026

--- config/fish/functions/ll.fish
+++ config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps='ls -lah' --wraps='lsdf -a || ls -la' --wraps='lsd -a || ls -la' --description 'alias ll=lsd -a || ls -la'
-    lsd -a || ls -la $argv
+function ll --wraps='ls -lah' --wraps='lsdf -a || ls -la' --wraps='lsd -a || ls -la' --wraps='lsd -la || ls -la' --description 'alias ll=lsd -la || ls -la'
+    lsd -la || ls -la $argv
 end
