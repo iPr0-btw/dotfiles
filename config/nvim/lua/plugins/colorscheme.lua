211dc6a3215d655f3b0306c752834ace9313f643
iPr0-btw <so23@duck.com>
2026-09-18 09:59:59 -0500

    auto-committed at Fri 18 Sep 09:59:59 CDT 2026

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -26,7 +26,7 @@ return {
           hl.Visual = { bg = "#283457" }
           hl.Pmenu = { bg = "#15161e" }
           hl.PmenuSel = { fg = "#000000", bg = "#7aa2f7" }
-          hl.Comment = { fg = "#EDACC2", bold = true, italic = true }
+          hl.Comment = { fg = "#444444", bold = true, italic = true }
           -- add more overrides
         end,
       })
