e9cf964351339ebef9ecbabd3fc79c194a82ea9a
iPr0-btw <so23@duck.com>
2026-09-18 10:13:02 -0500

    auto-committed at Fri 18 Sep 10:13:02 CDT 2026

---
+++ config/nvim/lua/plugins/colorscheme1.lua
@@ -0,0 +1,37 @@
+-- eturn {
+--  -- add onedark
+--  { "olimorris/onedark.nvim" },
+--  -- { "tiagovla/tokyodark.nvim" },
+--
+--  -- Configure LazyVim to load onedark
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "onedark_dark",
+--    },
+--  },
+--
+-- lua/plugins/colorscheme.lua
+return {
+  {
+    "folke/tokyonight.nvim",
+    enable = false,
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        on_highlights = function(hl)
+          hl.Normal = { bg = "#000000", fg = "#c0caf5" }
+          hl.NormalSB = { bg = "#000000", fg = "#c0caf5" }
+          hl.CursorLine = { bg = "#15161e" }
+          hl.LineNr = { fg = "#414868" }
+          hl.Visual = { bg = "#283457" }
+          hl.Pmenu = { bg = "#15161e" }
+          hl.PmenuSel = { fg = "#000000", bg = "#7aa2f7" }
+          hl.Comment = { fg = "#444444", bold = true }
+          -- add more overrides
+        end,
+      })
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+}
